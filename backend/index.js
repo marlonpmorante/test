4ae08ee3b2ab67060acd3ec75645ad167866042a
Faith <faithclairgregorio@gmail.com>
2025-10-16 01:54:09 +0800

    fix mysqlurl

--- backend/index.js
+++ backend/index.js
@@ -40,11 +40,11 @@ function getBaseUrl(req) {
 // DB_NAME=drugstore
 // DB_PORT=3306
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
