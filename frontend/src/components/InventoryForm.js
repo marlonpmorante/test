ffc036172dd441ca76721e7562fe8220bf61cda4
Faith <faithclairgregorio@gmail.com>
2025-10-21 09:48:11 +0800

    asjdj

--- frontend/src/components/InventoryForm.js
+++ frontend/src/components/InventoryForm.js
@@ -1014,7 +1014,6 @@ export default function InventoryForm() {
         }
 
         .ui-cart-section {
-          flex-grow: 1;
           display: flex;
           flex-direction: column;
           position: relative;
