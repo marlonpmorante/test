a62614155e19fe7b10efd1aaa5085dabc94499ef
Faith <faithclairgregorio@gmail.com>
2025-10-21 09:40:19 +0800

    asd

--- frontend/src/components/InventoryForm.js
+++ frontend/src/components/InventoryForm.js
@@ -1228,7 +1228,7 @@ export default function InventoryForm() {
               )}
             </div>
           
-            <div className="ui-summary-row" style={{ marginTop: '2px' }}>
+            <div className="ui-summary-row" style={{ marginTop: '50px' }}>
               <span>Total Price:</span>
               <span>₱{totalPrice.toFixed(2)}</span>
             </div>
